3c85466cd862925d07d21befe07fe5a68e8b7c04
suinbo <suin9610@gmail.com>
2023-12-13 08:52:45 +0900

    [Chore] 파일명 변경

--- src/app/pages/react-query-example/ReactQueryExample.tsx
+++ src/app/pages/react-query-example/ReactQueryExample.tsx
@@ -2,24 +2,24 @@ import { apiRequest, useFetches } from "@/utils/apis/request"
 import { useEffect, useState } from "react"
 
 const APIExample = () => {
-    const [hasToken, setHasToken] = useState<string>("")
+    const [hasToken, setHasToken] = useState<boolean>(false)
 
-    useEffect(() => setHasToken(localStorage.getItem("accessToken") as string), [])
+    useEffect(() => setHasToken(!!localStorage.getItem("accessToken")), [])
 
     const { data } = useFetches([
         {
             queryKey: ["CATEGORY"],
             queryFn: () => apiRequest("/browse/categories?country=KR"),
-            enabled: !!hasToken,
+            enabled: hasToken,
         },
         {
             queryKey: ["ALBUM"],
             queryFn: () => apiRequest("/albums/4aawyAB9vmqN3uQ7FjRGTy"),
-            enabled: !!hasToken,
+            enabled: hasToken,
         },
     ])
 
-    return <>{}</>
+    return <></>
 }
 
 export default APIExample
