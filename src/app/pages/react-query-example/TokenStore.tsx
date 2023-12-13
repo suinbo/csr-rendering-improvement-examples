49aa65e485d8968d236dea34549d504b8f26a958
suinbo <suin9610@gmail.com>
2023-12-13 10:22:01 +0900

    [Fix] TokenStore 렌더링 중 라우터 상태 업데이트 되지 않도록 처리
    - useQuery 의 onSuccess 옵션 사용

--- src/app/pages/react-query-example/TokenStore.tsx
+++ src/app/pages/react-query-example/TokenStore.tsx
@@ -4,7 +4,12 @@ import { useNavigate } from "react-router-dom"
 
 const TokenStore = () => {
     const navigate = useNavigate()
-    const { data: token, isLoading, isError } = useQuery("token", fetchToken)
+    const { isLoading, isError } = useQuery("token", fetchToken, {
+        onSuccess: token => {
+            navigate("/react-query-test")
+            localStorage.setItem("accessToken", token)
+        },
+    })
 
     if (isLoading) {
         return <p>Loading...</p>
@@ -12,11 +17,6 @@ const TokenStore = () => {
 
     if (isError) {
         return <p>Error fetching token</p>
-    }
-
-    if (token) {
-        navigate("/api-test")
-        localStorage.setItem("accessToken", token)
     }
 
     return <></>
